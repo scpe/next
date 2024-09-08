f0ce53560c2e1f44dababa2b83ee8265f65d3403
scott pederson <scpe@users.noreply.github.com>
2024-09-08 15:12:38 -0500

    add all

---
+++ custom.d.ts
@@ -0,0 +1,15 @@
+interface WeatherProps {
+    weather: string;
+}
+
+type WeatherDetailType = {
+    zipcode: string;
+    weather: string;
+    temp?: number;
+};
+
+type responseItemType = {
+    id: string;
+    name: string;
+};
+
---
+++ graphql/resolvers.ts
@@ -0,0 +1,20 @@
+import { WeatherInterface } from "../mongoose/weather/interface";
+import {findByZip, updateByZip} from "../mongoose/weather/services" ; 
+
+
+export const resolvers = {
+    Query: {
+        weather: async (_: any, param: WeatherInterface) => {
+            let data = await findByZip(param.zip) ;
+            return [data];
+        },
+    },
+    Mutation: {
+        weather: async (_: any, param: {data: WeatherInterface}) => {
+        await updateByZip(param.data.zip, param.data );
+            
+        let data = await findByZip(param.data.zip);
+        return [data];
+        }
+    }
+}
